47c85a0671762b0b65e3207178827fa52529bde2
Gilang <gilangnurrizki173@gmail.com>
2024-07-10 18:02:19 +0700

    gradient

--- app/src/main/java/com/example/nutrilab/model/FoodResponse.java
+++ app/src/main/java/com/example/nutrilab/model/FoodResponse.java
@@ -8,17 +8,8 @@ public class FoodResponse {
     public int getStatus() {
         return status;
     }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     public FoodData getData() {
@@ -37,16 +28,8 @@ public class FoodResponse {
             return foodInfo;
         }
 
-        public void setFoodInfo(FoodInfo foodInfo) {
-            this.foodInfo = foodInfo;
-        }
-
         public ProgressNutrition getProgressNutrition() {
             return progressNutrition;
-        }
-
-        public void setProgressNutrition(ProgressNutrition progressNutrition) {
-            this.progressNutrition = progressNutrition;
         }
 
         public class FoodInfo {
@@ -62,56 +45,28 @@ public class FoodResponse {
                 return foodName;
             }
 
-            public void setFoodName(String foodName) {
-                this.foodName = foodName;
-            }
-
             public String getFoodInformation() {
                 return foodInformation;
-            }
-
-            public void setFoodInformation(String foodInformation) {
-                this.foodInformation = foodInformation;
             }
 
             public String getCalorie() {
                 return calorie;
             }
 
-            public void setCalorie(String calorie) {
-                this.calorie = calorie;
-            }
-
             public String getSugar() {
                 return sugar;
-            }
-
-            public void setSugar(String sugar) {
-                this.sugar = sugar;
             }
 
             public String getCarbohydrate() {
                 return carbohydrate;
             }
 
-            public void setCarbohydrate(String carbohydrate) {
-                this.carbohydrate = carbohydrate;
-            }
-
             public String getFat() {
                 return fat;
             }
 
-            public void setFat(String fat) {
-                this.fat = fat;
-            }
-
             public String getProtein() {
                 return protein;
-            }
-
-            public void setProtein(String protein) {
-                this.protein = protein;
             }
         }
 
@@ -126,41 +81,22 @@ public class FoodResponse {
                 return totalCalories;
             }
 
-            public void setTotalCalories(int totalCalories) {
-                this.totalCalories = totalCalories;
-            }
-
             public int getTotalCarbohydrate() {
                 return totalCarbohydrate;
-            }
-
-            public void setTotalCarbohydrate(int totalCarbohydrate) {
-                this.totalCarbohydrate = totalCarbohydrate;
             }
 
             public int getTotalProtein() {
                 return totalProtein;
             }
 
-            public void setTotalProtein(int totalProtein) {
-                this.totalProtein = totalProtein;
-            }
-
             public int getTotalFat() {
                 return totalFat;
-            }
-
-            public void setTotalFat(int totalFat) {
-                this.totalFat = totalFat;
             }
 
             public int getTotalSugar() {
                 return totalSugar;
             }
 
-            public void setTotalSugar(int totalSugar) {
-                this.totalSugar = totalSugar;
-            }
         }
     }
 }
